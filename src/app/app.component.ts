a4d4dc0d33ee2d30a88121241bf48694b2bc286d
poomipat-k <k.poomipat@gmail.com>
2023-11-01 20:57:38 +0700

    review add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, NavbarComponent],
 })
 export class AppComponent {
   title = 'ui-running-fund';
