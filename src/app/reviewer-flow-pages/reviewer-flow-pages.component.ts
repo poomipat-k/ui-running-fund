3199fb89b226a8658e1203b2bf37fafe5c64e35b
poomipat-k <k.poomipat@gmail.com>
2023-11-13 23:57:10 +0700

    review add prelim review score questions section

--- src/app/reviewer-flow-pages/reviewer-flow-pages.component.ts
+++ src/app/reviewer-flow-pages/reviewer-flow-pages.component.ts
@@ -89,6 +89,7 @@ export class ReviewerFlowPagesComponent implements OnInit {
     this.projectService.getReviewCriteria(1).subscribe((result) => {
       if (result) {
         this.reviewCriteriaList = result;
+        console.log(this.reviewCriteriaList);
       }
     });
   }
--- src/app/reviewer-flow-pages/reviewer-score/reviewer-score.component.ts
+++ src/app/reviewer-flow-pages/reviewer-score/reviewer-score.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ReviewCriteria } from '../../shared/models/review-criteria';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-reviewer-score',
@@ -11,4 +13,41 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class ReviewerScoreComponent {
   @Input() form: FormGroup;
+  @Input() criteriaList: ReviewCriteria[] = [];
+
+  protected groupHeaders = [
+    {
+      id: 1,
+      groupName: '1. ความเป็นมา หลักการและเหตุผล มีความชัดเจนหรือไม่ อย่างไร',
+    },
+    {
+      id: 2,
+      groupName: '2. มาตรฐานการจัดงานวิ่งเพื่อสุขภาพ',
+    },
+    {
+      id: 3,
+      groupName:
+        '3. แนวทางและภาพลักษณ์ที่สอดคล้องสำนักงานกองทุนสนับสนุนการสร้างเสริมสุขภาพ (สสส.)',
+    },
+    {
+      id: 4,
+      groupName: '4. ประโยชน์ของการนำเสนอองค์กร สสส. ในการสนับสนุนทุนอุปถัมภ์',
+    },
+    {
+      id: 5,
+      groupName: '5. ความน่าเชื่อถือและประสบการณ์การจัดงาน',
+    },
+    {
+      id: 6,
+      groupName: '6. งบประมาณที่ขอรับการสนับสนุน และผลที่คาดว่าจะได้รับ',
+    },
+  ];
+
+  protected readonly sanitizer: DomSanitizer = inject(DomSanitizer);
+
+  protected buildQuestionText(criteria: ReviewCriteria): string {
+    return `${criteria.group_number}.${criteria.in_group_number} ${criteria.display_text}`;
+  }
+
+  constructor() {}
 }
