e65dcf2ec071e6b2c1ddd06c600cc964a8c6a1eb
poomipat-k <k.poomipat@gmail.com>
2024-01-15 21:19:55 +0700

    applicant init progress bar

--- src/app/applicant-flow-pages/applicant-flow-pages.component.ts
+++ src/app/applicant-flow-pages/applicant-flow-pages.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ProgressBarStepsComponent } from '../components/progress-bar-steps/progress-bar-steps.component';
 
 @Component({
   selector: 'app-applicant-flow-pages',
   standalone: true,
-  imports: [],
+  imports: [ProgressBarStepsComponent],
   templateUrl: './applicant-flow-pages.component.html',
-  styleUrl: './applicant-flow-pages.component.scss'
+  styleUrl: './applicant-flow-pages.component.scss',
 })
-export class ApplicantFlowPagesComponent {
-
-}
+export class ApplicantFlowPagesComponent {}
---
+++ src/app/components/progress-bar-steps/progress-bar-steps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com-progress-bar-steps',
+  standalone: true,
+  imports: [],
+  templateUrl: './progress-bar-steps.component.html',
+  styleUrl: './progress-bar-steps.component.scss',
+})
+export class ProgressBarStepsComponent {}
