eb8ab123769e24edbb92e2bb6f77cab4bde6b5e8
poomipat-k <k.poomipat@gmail.com>
2024-01-16 09:53:20 +0700

    applicant steps progress bar component

--- src/app/applicant-flow-pages/applicant-flow-pages.component.ts
+++ src/app/applicant-flow-pages/applicant-flow-pages.component.ts
@@ -8,4 +8,24 @@ import { ProgressBarStepsComponent } from '../components/progress-bar-steps/prog
   templateUrl: './applicant-flow-pages.component.html',
   styleUrl: './applicant-flow-pages.component.scss',
 })
-export class ApplicantFlowPagesComponent {}
+export class ApplicantFlowPagesComponent {
+  protected progressBarSteps = [
+    ['ข้อมูลพื้นฐานโครงการ'],
+    ['ข้อมูลการติดต่อ'],
+    ['ข้อมูลข้อเสนอโครงการ', 'และแผนบริหารจัดการ'],
+    ['ประสบการณ์', 'ดำเนินการโครงการ'],
+    ['ความต้องการ', 'ขอรับการสนับสนุน'],
+    ['เอกสารแนบเพิ่มเติม', '(ถ้ามี)'],
+    ['ยืนยัน'],
+  ];
+
+  protected currentStep = 0;
+
+  addStep() {
+    this.currentStep += 1;
+  }
+
+  reduceStep() {
+    this.currentStep -= 1;
+  }
+}
--- src/app/components/progress-bar-steps/progress-bar-steps.component.ts
+++ src/app/components/progress-bar-steps/progress-bar-steps.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-com-progress-bar-steps',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './progress-bar-steps.component.html',
   styleUrl: './progress-bar-steps.component.scss',
 })
-export class ProgressBarStepsComponent {}
+export class ProgressBarStepsComponent {
+  @Input() currentStep = 0;
+  @Input() startStep = 1;
+  @Input() steps: string[][] = [];
+}
