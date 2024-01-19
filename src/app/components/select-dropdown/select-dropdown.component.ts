8eda8f86c7ee631b433a0aad1092654ae8e03f45
poomipat-k <k.poomipat@gmail.com>
2024-01-20 00:30:18 +0700

    applicant init custom dropdown

--- src/app/components/select-dropdown/select-dropdown.component.ts
+++ src/app/components/select-dropdown/select-dropdown.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ArrowDropdownComponent } from '../svg/arrow-dropdown/arrow-dropdown.component';
 
 @Component({
   selector: 'app-com-select-dropdown',
   standalone: true,
-  imports: [],
+  imports: [ArrowDropdownComponent],
   templateUrl: './select-dropdown.component.html',
   styleUrl: './select-dropdown.component.scss',
 })
-export class SelectDropdownComponent {}
+export class SelectDropdownComponent {
+  @Input() placeholder = 'เลือกการค้นหา';
+  protected showDropdown = false;
+}
---
+++ src/app/components/svg/arrow-dropdown/arrow-dropdown.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com-svg-arrow-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrow-dropdown.component.html',
+  styleUrl: './arrow-dropdown.component.scss',
+})
+export class ArrowDropdownComponent {}
