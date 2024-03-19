613a7638646bfe53ff74068d2197cc132a6be5ec
poomipat-k <k.poomipat@gmail.com>
2024-03-19 23:20:26 +0700

    admin close dropdown when clicked outside

--- src/app/components/select-dropdown-template/select-dropdown-template.component.ts
+++ src/app/components/select-dropdown-template/select-dropdown-template.component.ts
@@ -1,6 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { ColumnTypeEnum } from '../../shared/enums/column-type';
 import { RadioOption } from '../../shared/models/radio-option';
 import { TableCellTemplateComponent } from '../table-cell-template/table-cell-template.component';
@@ -12,7 +22,10 @@ import { TableCellTemplateComponent } from '../table-cell-template/table-cell-te
   templateUrl: './select-dropdown-template.component.html',
   styleUrl: './select-dropdown-template.component.scss',
 })
-export class SelectDropdownTemplateComponent {
+export class SelectDropdownTemplateComponent implements OnInit, OnDestroy {
+  @ViewChild('selectDropdownButton') selectDropdownButton: ElementRef;
+  @ViewChild('inputIcon') inputIcon: ElementRef;
+
   @Input() items: RadioOption[] = [];
   @Input() form: FormGroup;
   @Input() controlName = '';
@@ -24,13 +37,36 @@ export class SelectDropdownTemplateComponent {
   @Input() disabled = false;
   @Input() onChange: () => void;
 
-  @ViewChild('selectDropdownButton') selectDropdownButton: ElementRef;
+  private listenerFn = () => {};
+  private renderer: Renderer2 = inject(Renderer2);
+  private readonly subs: Subscription[] = [];
+
   protected cellType = ColumnTypeEnum.Badge;
 
   protected showDropdown = false;
 
   get selectedValue() {
     return this.form.get(this.controlName)?.value || this.defaultValue;
+  }
+
+  ngOnInit(): void {
+    /*
+    renderer.listen returns () => void. 
+    Which is an unsubscribe function to be used when component destroyed to unsubscribe registered event.
+    */
+    this.listenerFn = this.renderer.listen('window', 'click', (e: Event) => {
+      const clickedOutside = this.selectDropdownButton.nativeElement.contains(
+        e.target
+      );
+      if (this.showDropdown && !clickedOutside) {
+        this.hideDropdown();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.listenerFn();
+    this.subs.forEach((s) => s.unsubscribe());
   }
 
   onButtonClicked() {
