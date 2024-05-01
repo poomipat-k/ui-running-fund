0935d2f1a97d302dfbca4e475f070727ec6d4de6
poomipat-k <k.poomipat@gmail.com>
2024-05-01 12:53:49 +0700

    admin init asking for help page content without form

--- src/app/asking-for-help/asking-for-help.component.ts
+++ src/app/asking-for-help/asking-for-help.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaqComponent } from '../components/faq/faq.component';
 
 @Component({
   selector: 'app-asking-for-help',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FaqComponent],
   templateUrl: './asking-for-help.component.html',
   styleUrl: './asking-for-help.component.scss',
 })
