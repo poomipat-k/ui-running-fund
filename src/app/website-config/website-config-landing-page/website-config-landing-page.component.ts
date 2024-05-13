e599d02ea41063960c710b9835ed9bdb2117b6ca
poomipat-k <k.poomipat@gmail.com>
2024-05-13 19:00:32 +0700

    admin refactor rich text editor

--- src/app/website-config/website-config-landing-page/website-config-landing-page.component.ts
+++ src/app/website-config/website-config-landing-page/website-config-landing-page.component.ts
@@ -21,6 +21,18 @@ import { SafeHtmlPipe } from '../../shared/pipe/safe-html.pipe';
 export class WebsiteConfigLandingPageComponent implements OnInit {
   form: FormGroup;
 
+  protected editorInit = {
+    base_url: '/tinymce',
+    suffix: '.min',
+    font_size_formats:
+      '8pt 10pt 12pt 14pt 16pt 18pt 20pt 24pt 30pt 36pt 48pt 60pt 72pt',
+    images_file_types: 'jpeg,jpg,jpe,jfi,jif,jfif,png,gif,bmp,webp,svg',
+  };
+  protected editorPlugins =
+    'preview autolink autosave save code visualblocks visualchars fullscreen image link media codesample table charmap nonbreaking anchor lists advlist wordcount help charmap quickbars emoticons';
+  protected editorToolbar =
+    'undo redo | blocks fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | anchor codesample';
+
   get dataGroup(): FormGroup {
     return this.form.get('data') as FormGroup;
   }
