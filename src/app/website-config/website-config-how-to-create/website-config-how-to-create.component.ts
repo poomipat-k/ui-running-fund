8cccb3b68a8e3e6653c97fcb2fbe954e5f4928d7
poomipat-k <k.poomipat@gmail.com>
2024-05-28 00:20:55 +0700

    admin refactor tinyMCE editor to common component

--- src/app/website-config/website-config-how-to-create/website-config-how-to-create.component.ts
+++ src/app/website-config/website-config-how-to-create/website-config-how-to-create.component.ts
@@ -1,24 +1,13 @@
-import { Component, Input, ViewChild, inject } from '@angular/core';
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { EditorComponent, EditorModule } from '@tinymce/tinymce-angular';
-import { concatMap, lastValueFrom, map } from 'rxjs';
+import { Component, Input, ViewChild } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CancelConfirmModalComponent } from '../../components/cancel-confirm-modal/cancel-confirm-modal.component';
 import { CustomEditorComponent } from '../../components/custom-editor/custom-editor.component';
 import { InputTextComponent } from '../../components/input-text/input-text.component';
-import { S3Service } from '../../services/s3.service';
 
 @Component({
   selector: 'app-website-config-how-to-create',
   standalone: true,
   imports: [
-    ReactiveFormsModule,
-    EditorModule,
     InputTextComponent,
     CancelConfirmModalComponent,
     CustomEditorComponent,
@@ -34,18 +23,6 @@ export class WebsiteConfigHowToCreateComponent {
   protected removingItemIndex = 0;
 
   @Input() formArray: FormArray;
-
-  private readonly s3Service: S3Service = inject(S3Service);
-
-  protected editorInit: EditorComponent['init'] = {};
-  protected editorPlugins =
-    'preview autolink autosave save code visualblocks visualchars fullscreen image link media codesample table charmap nonbreaking anchor lists advlist wordcount help charmap quickbars emoticons';
-  protected editorToolbar =
-    'undo redo | blocks fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | anchor codesample';
-
-  ngOnInit(): void {
-    this.initRichTextEditor();
-  }
 
   getFormGroup(index: number): FormGroup {
     return this.formArray.at(index) as FormGroup;
@@ -80,43 +57,5 @@ export class WebsiteConfigHowToCreateComponent {
   doRemoveFormItem() {
     this.formArray.removeAt(this.removingItemIndex);
     this.deleteItemModalComponent.closeModal();
-  }
-
-  private initRichTextEditor() {
-    this.editorInit = {
-      base_url: '/tinymce',
-      suffix: '.min',
-      font_size_formats: '10px 12px 14px 16px 18px 20px 24px 28px 32px 48px',
-      line_height_formats:
-        '10px 16px 18px 20px 24px 27px 30px 33px 36px 40px 48px',
-      images_file_types: 'jpeg,jpg,jpe,jfi,jif,jfif,png,gif,bmp,webp,svg',
-      images_reuse_filename: true,
-      image_advtab: true,
-      block_unsupported_drop: true,
-      content_style: 'img {max-width: 100%;}',
-      images_upload_handler: (blobInfo) => {
-        const objectKey = `cms/how_to_create/${Date.now()}-${blobInfo.filename()}`;
-        const file = new File([blobInfo.blob()], objectKey);
-        const promise = lastValueFrom(
-          this.s3Service.getPutPresigned(objectKey).pipe(
-            concatMap((putPresignedObject) => {
-              return this.s3Service
-                .putPresigned(putPresignedObject.presigned.URL, file, {
-                  headers: {
-                    'Content-Type':
-                      blobInfo.blob()?.type ?? 'application/octet-stream',
-                  },
-                })
-                .pipe(
-                  map(() => {
-                    return putPresignedObject.fullPath;
-                  })
-                );
-            })
-          )
-        );
-        return promise;
-      },
-    };
   }
 }
