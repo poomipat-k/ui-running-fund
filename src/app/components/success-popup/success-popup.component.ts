1067ea41af2ad80bf540d68cd721dc11e0a36021
poomipat-k <k.poomipat@gmail.com>
2024-08-19 13:23:36 +0700

    feature/admin before refactor component that use popup component

--- src/app/components/success-popup/success-popup.component.ts
+++ src/app/components/success-popup/success-popup.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { SvgCheckComponent } from '../svg/svg-check/svg-check.component';
@@ -8,7 +9,46 @@ import { SvgCheckComponent } from '../svg/svg-check/svg-check.component';
   imports: [CommonModule, SvgCheckComponent],
   templateUrl: './success-popup.component.html',
   styleUrl: './success-popup.component.scss',
+  animations: [
+    trigger('popup', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%) scale(0)',
+        }),
+        animate(
+          '300ms ease-out',
+          style({
+            opacity: 1,
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%) scale(1)',
+          })
+        ),
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 1,
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%) scale(1)',
+        }),
+        animate(
+          '300ms ease-out',
+          style({
+            opacity: 0,
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%) scale(0)',
+          })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class SuccessPopupComponent {
+  @Input() show = false;
   @Input() displayText: string;
 }
